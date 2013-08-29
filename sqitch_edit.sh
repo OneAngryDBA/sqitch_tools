da6e13e46cce3da5cb0f8fd7d472c7722ba1f027
Matt Brennan <matt.brennan@oneangrydba.com>
2013-08-29 14:55:49 -0400

    Order changed.

--- sqitch_edit.sh
+++ sqitch_edit.sh
@@ -14,24 +14,24 @@ if [ $IS_SQITCH_PROJECT != 000 ]; then
 	echo $CURRENT_DIRECTORY
 	
 	for param in "$@"; do
-		DEPLOY_FILE="$CURRENT_DIRECTORY/deploy/$param.sql"
-		echo $DEPLOY_FILE
 		REVERT_FILE="$CURRENT_DIRECTORY/revert/$param.sql"
 		echo $REVERT_FILE
+		DEPLOY_FILE="$CURRENT_DIRECTORY/deploy/$param.sql"
+		echo $DEPLOY_FILE
 		VERIFY_FILE="$CURRENT_DIRECTORY/verify/$param.sql"
 		echo $VERIFY_FILE
-		
-		if [ -f $DEPLOY_FILE ]; then
-			echo $DEPLOY_FILE
-			FILES_TO_OPEN="$FILES_TO_OPEN $DEPLOY_FILE"
+				
+		if [ -f $REVERT_FILE ]; then
+			echo $REVERT_FILE
+			FILES_TO_OPEN="$FILES_TO_OPEN $REVERT_FILE"
 		else
 			echo "Deploy file for sqitch change $param does not exist."
 			exit
 		fi
-		
-		if [ -f $REVERT_FILE ]; then
-			echo $REVERT_FILE
-			FILES_TO_OPEN="$FILES_TO_OPEN $REVERT_FILE"
+
+		if [ -f $DEPLOY_FILE ]; then
+			echo $DEPLOY_FILE
+			FILES_TO_OPEN="$FILES_TO_OPEN $DEPLOY_FILE"
 		else
 			echo "Deploy file for sqitch change $param does not exist."
 			exit
