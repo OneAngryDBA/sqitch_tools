f1a651527f7fc16b1e399cdd2a815b5865ab05c3
Matt Brennan <matt.brennan@oneangrydba.com>
2013-08-29 13:44:06 -0400

    Testing multiple changes.

--- sqitch_edit.sh
+++ sqitch_edit.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
 
-subl -n revert/$1.sql deploy/$1.sql verify/$1.sql
+FILES_TO_OPEN=''
+
+for param in "$@"; do
+	FILES_TO_OPEN.="revert/$1.sql deploy/$1.sql verify/$1.sql "
+done
+
+subl -n $FILES_TO_OPEN
